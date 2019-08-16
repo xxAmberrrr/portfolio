cc53268351a0ea0c9450ce699c141b67345e27e3
Amber <amber__12@hotmail.com>
2019-08-16 22:23:55 +0200

    Add intro

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ export default class App extends React.Component {
     return (
       <div className="App">
       <header className="App-header">
-        <h1>Amber's portfolio</h1>
+        <div className="introTxt">
+          <h1>Amber Hoogland</h1>
+          <p class="introParagraph">
+            full-stack developer met een passie voor front-end | 22 jaar oud | Capelle a/d IJssel | klantvriendelijk | doorzetter | leergierig | structuur-junkie
+          </p>
+        </div>
+        <div className="profileImgContainer">
+        <div className="profilePic"></div>
+        </div>
       </header>
     </div>
     )
