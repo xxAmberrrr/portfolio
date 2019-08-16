7cf0371e54efb152cbe34550c5ee8d023389350a
Amber <amber__12@hotmail.com>
2019-08-16 20:59:06 +0200

    Remove react stuff and add sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/main.scss';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Amber's portfolio</h1>
       </header>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
